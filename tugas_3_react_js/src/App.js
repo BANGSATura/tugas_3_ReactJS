f42caa6eebbcb18fcc1418c7215e2442947824b4
STR <saturasstotenkopf@gmail.com>
2020-02-05 12:24:39 +0700

    Tugas 3 React JS

--- tugas_3_react_js/src/App.js
+++ tugas_3_react_js/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Footer from './Page/Footer'
 import MenuMakanan from './Page/MenuMakanan'
 import Header from './Page/Header'
+import MenuTentangKami from './Page/MenuTentangKami'
+import MenuKontak from './Page/MenuKontak'
 class App extends Component {
   render() {
     return (
@@ -9,6 +11,8 @@ class App extends Component {
         <Header/>
           <h2>Daftar Makananan Yang Kami Sediakan:</h2>
           <MenuMakanan/>
+          <MenuTentangKami/>
+          <MenuKontak/>
         <Footer/>
       </div>
     );
